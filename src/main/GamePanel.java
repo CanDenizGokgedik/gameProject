4fb30707859ed370c8081b6d7c9099d2d1832f33
CanDenizGokgedik <20050111057@ybu.edu.tr>
2023-01-22 13:28:23 +0300

    main updates

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -6,5 +6,10 @@ public class GamePanel extends JPanel {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+
+    
 
 }
